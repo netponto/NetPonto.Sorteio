a3ddb3d6fcc1349bd40b310cbb70fe680be72a2d
Ricardo Alves <ricardoloboalves@gmail.com>
2011-05-24 13:44:16 +0100

    Normalização dos whitespaces dos ficheiros, não foi feita nenhuma alteração ao código

--- src/SilverlightApp/Controls/TagItem.cs
+++ src/SilverlightApp/Controls/TagItem.cs
@@ -2,71 +2,72 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+
 using Axelerate.Silverlight3D.Media.Media3D;
 
 namespace SilverlightApp.Controls
 {
-	public class TagItem : Label
-	{
-		public TagItem(double x, double y, double z, string text)
-		{
-			BorderThickness = new Thickness(0, 0, 0, 0);
-			BorderBrush = new SolidColorBrush(Colors.Black);
-			Foreground = new SolidColorBrush(Color.FromArgb(255, 222, 194, 7));
-			Padding = new Thickness(5, 5, 5, 5);
-			FontFamily = new FontFamily("Segoe UI Bold");
+    public class TagItem : Label
+    {
+        public TagItem(double x, double y, double z, string text)
+        {
+            BorderThickness = new Thickness(0, 0, 0, 0);
+            BorderBrush = new SolidColorBrush(Colors.Black);
+            Foreground = new SolidColorBrush(Color.FromArgb(255, 222, 194, 7));
+            Padding = new Thickness(5, 5, 5, 5);
+            FontFamily = new FontFamily("Segoe UI Bold");
             Content = text;
-			CenterPoint = new Point3D(x, y, z);
-		}
+            CenterPoint = new Point3D(x, y, z);
+        }
 
-		public void Redraw(double xOffset, double yOffset)
-		{
-			var posZ = CenterPoint.Z + 200;
+        public void Redraw(double xOffset, double yOffset)
+        {
+            var posZ = CenterPoint.Z + 200;
 
-			this.FontSize = Math.Max(10 * (posZ / 100), 1);
-			var alpha = CenterPoint.Z + 200;
-			if (alpha > 255)
-			{
-			    alpha = 255;
-			}
+            this.FontSize = Math.Max(10 * (posZ / 100), 1);
+            var alpha = CenterPoint.Z + 200;
+            if (alpha > 255)
+            {
+                alpha = 255;
+            }
 
-			if (alpha < 0)
-			{
-			    alpha = 0;
-			}
+            if (alpha < 0)
+            {
+                alpha = 0;
+            }
 
-			Foreground = new SolidColorBrush(Color.FromArgb(Convert.ToByte(alpha), 222, 194, 7));
-			Canvas.SetLeft(this, CenterPoint.X + xOffset - (this.ActualWidth / 2));
-			Canvas.SetTop(this, -CenterPoint.Y + yOffset - (this.ActualHeight / 2));
-			Canvas.SetZIndex(this, Convert.ToInt32(CenterPoint.Z));
-		}
+            Foreground = new SolidColorBrush(Color.FromArgb(Convert.ToByte(alpha), 222, 194, 7));
+            Canvas.SetLeft(this, CenterPoint.X + xOffset - (this.ActualWidth / 2));
+            Canvas.SetTop(this, -CenterPoint.Y + yOffset - (this.ActualHeight / 2));
+            Canvas.SetZIndex(this, Convert.ToInt32(CenterPoint.Z));
+        }
 
-		public void SetAsTop(bool isTop)
-		{
-			if (isTop)
-			{
-				BorderThickness = new Thickness(2, 2, 2, 2);
-				Background = new SolidColorBrush(Color.FromArgb(75, 212,207,80));
-				Foreground = new SolidColorBrush(Color.FromArgb(255, 222,194,7));
-				BorderBrush = new SolidColorBrush(Color.FromArgb(255, 124,96,45));
-			}
-			else
-			{
-				BorderThickness = new Thickness(0, 0, 0, 0);
-				Background = new SolidColorBrush(Colors.Transparent);
-				Foreground = new SolidColorBrush(Color.FromArgb(255, 222, 194, 7));
-			}
-		}
+        public void SetAsTop(bool isTop)
+        {
+            if (isTop)
+            {
+                BorderThickness = new Thickness(2, 2, 2, 2);
+                Background = new SolidColorBrush(Color.FromArgb(75, 212, 207, 80));
+                Foreground = new SolidColorBrush(Color.FromArgb(255, 222, 194, 7));
+                BorderBrush = new SolidColorBrush(Color.FromArgb(255, 124, 96, 45));
+            }
+            else
+            {
+                BorderThickness = new Thickness(0, 0, 0, 0);
+                Background = new SolidColorBrush(Colors.Transparent);
+                Foreground = new SolidColorBrush(Color.FromArgb(255, 222, 194, 7));
+            }
+        }
 
-		public string SetAsWinner()
-		{
-			BorderThickness = new Thickness(2, 2, 2, 2);
-			Background = new SolidColorBrush(Color.FromArgb(75, 205, 212, 80));
-			Foreground = new SolidColorBrush(Color.FromArgb(255, 229, 31, 31));
-			BorderBrush = new SolidColorBrush(Color.FromArgb(255, 159, 31, 31));
-			return Content.ToString();
-		}
+        public string SetAsWinner()
+        {
+            BorderThickness = new Thickness(2, 2, 2, 2);
+            Background = new SolidColorBrush(Color.FromArgb(75, 205, 212, 80));
+            Foreground = new SolidColorBrush(Color.FromArgb(255, 229, 31, 31));
+            BorderBrush = new SolidColorBrush(Color.FromArgb(255, 159, 31, 31));
+            return Content.ToString();
+        }
 
-		public Point3D CenterPoint { get; set; }
-	}
+        public Point3D CenterPoint { get; set; }
+    }
 }
--- src/SilverlightApp/MainPage.xaml.cs
+++ src/SilverlightApp/MainPage.xaml.cs
@@ -8,71 +8,71 @@ using SilverlightApp.Controls;
 
 namespace SilverlightApp
 {
-	public partial class MainPage : UserControl
-	{
+    public partial class MainPage : UserControl
+    {
         static readonly string[] participantes =
             {
-				"Bruno Lopes",
-				"Bárbara Castilho",
-				"Caio Proiete",
-				"Dmitry Ossipov",
-				"Henrry Pires",
-				"João Manso",
-				"Jorge Silva",
-				"Nuno Gomes",
-				"Paulo Correia",
-				"Paulo Morgado",
-				"Pedro Rosa",
-				"Sara Silva",
-				"Ricardo Alves",
-				"Virgílio Raposo",
+                "Bruno Lopes",
+                "Bárbara Castilho",
+                "Caio Proiete",
+                "Dmitry Ossipov",
+                "Henrry Pires",
+                "João Manso",
+                "Jorge Silva",
+                "Nuno Gomes",
+                "Paulo Correia",
+                "Paulo Morgado",
+                "Pedro Rosa",
+                "Sara Silva",
+                "Ricardo Alves",
+                "Virgílio Raposo",
             };
 
-		public MainPage()
-		{
-			InitializeComponent();
+        public MainPage()
+        {
+            InitializeComponent();
 
             ParticipantsList.FontSize = 20;
 
-		    ParticipantsList.Text = String
-		        .Join(Environment.NewLine, participantes);
+            ParticipantsList.Text = String
+                .Join(Environment.NewLine, participantes);
 
-			this.KeyDown += MainPage_KeyDown;
-		}
+            this.KeyDown += MainPage_KeyDown;
+        }
 
-		void MainPage_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
-		{
-			if (e.Key == Key.Escape)
-			{
-				if (Application.Current.IsRunningOutOfBrowser)
-				{
-					Application.Current.MainWindow.Close();
-				}
-			}
-		}
+        void MainPage_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                if (Application.Current.IsRunningOutOfBrowser)
+                {
+                    Application.Current.MainWindow.Close();
+                }
+            }
+        }
 
-		private void Sortear_Click(object sender, RoutedEventArgs e)
-		{
-			this.LayoutRoot.Children.Clear();
-			this.LayoutRoot.RowDefinitions.Clear();
+        private void Sortear_Click(object sender, RoutedEventArgs e)
+        {
+            this.LayoutRoot.Children.Clear();
+            this.LayoutRoot.RowDefinitions.Clear();
 
-			var memberInput = new List<string>();
-			
+            var memberInput = new List<string>();
+
             TextReader reader = new StringReader(ParticipantsList.Text = String
-		        .Join(Environment.NewLine, participantes));
-			
-            string line;
-			while ((line = reader.ReadLine()) != null)
-			{
-				memberInput.Add(line);
-			}
+                .Join(Environment.NewLine, participantes));
 
-			this.LayoutRoot.Children.Add(new TagRandomizer(memberInput)
-			                             	{   VerticalAlignment = System.Windows.VerticalAlignment.Stretch,
-			                             		HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch
-											});
-			// Go full screen
-			Application.Current.Host.Content.IsFullScreen = true;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                memberInput.Add(line);
+            }
+
+            this.LayoutRoot.Children.Add(new TagRandomizer(memberInput) {
+                VerticalAlignment = System.Windows.VerticalAlignment.Stretch,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch
+            });
+            // Go full screen
+            Application.Current.Host.Content.IsFullScreen = true;
         }
     }
 }
